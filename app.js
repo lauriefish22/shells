54885b83902b7e4d50cfb53931e7da3a1dac8f19
Laurie Fish <iamlauriefish@gmail.com>
2023-01-10 19:20:43 -0800

    fixed functions

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const elmo = document.getElementById('elmo');
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
+console.log('bb');
+
 
 let correctGuesses = 0;
 let totalGuesses = 0;
@@ -23,13 +25,29 @@ cmButton.addEventListener('click', () => {});
 
 elmoButton.addEventListener('click', () => {});
 
-function clickLocation ()
-function pickRandomPlace () {
-    const places = ['bigBird'. 'cookiemonster', 'elmo'];
+function clickLocation ();
+function removeClasses ();
+const correctPlace = pickRandomPlace();
+const correctSectionEl = document.getElementById{correctPlace}-section);
+
+correctSectionEl.classList.add('cookie');
+
+if (correctPlace === placeTheUserClicked) {
+    correctGuesses++;
+} else {
+    incorrectGuesses++;
 }
-    const index = Math.floor(Math.random() * places.length);
-    const corrrectPlace = places[index];
-    return corrrectPlace;
+totalGuesses++
+
+winsEl.textContent = correctGuesses;
+lossesEl.textContent = incorrectGuesses;
+totalEl.textContent = totalGuesses;
+
+    function pickRandomPlace () {
+        const places = ['bigBird'. 'cookiemonster', 'elmo'];
+        const index = Math.floor(Math.random() * places.length);
+        const correctPlace = places[index];
+        return correctPlace;}
 /* State */
 
 /* Events */

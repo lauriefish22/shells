290e93e57ea4171b1ec4654a20deee0e8e8c455b
Laurie Fish <iamlauriefish@gmail.com>
2023-01-10 18:00:57 -0800

    added const and event listeners to js
    ls

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 /* Imports */
 
 /* Get DOM Elements */
+const bbButton = document.getElementById('bb-button');
+const cmButton = document.getElementById('cm-button');
+const elmoButton = document.getElementById('elmo-button');
+
+const bigBird = document.getElementById('bb');
+const cookiemonster = document.getElementById('cm');
+const elmo = document.getElementById('elmo');
+
+const totalEl = document.getElementById('total');
+const lossesEl = document.getElementById('losses');
+const winsEl = document.getElementById('wins');
+
+let correctGuesses = 0;
+let total Guesses = 0;
+
+bbButton.addEventListener('click', () => {})
+
+cmButton.addEventListener('click', () => {})
+
+elmoButton.addEventListener('click', () {})
 
 /* State */
 

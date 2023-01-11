1801f67aa84167cb01b08bfa9e8aa16b5bbb6db1
Laurie Fish <iamlauriefish@gmail.com>
2023-01-10 19:03:04 -0800

    added functions

--- app.js
+++ app.js
@@ -14,14 +14,22 @@ const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
 
 let correctGuesses = 0;
-let total Guesses = 0;
+let totalGuesses = 0;
+let incorrectGuesses = 0;
 
-bbButton.addEventListener('click', () => {})
+bbButton.addEventListener('click', () => {});
 
-cmButton.addEventListener('click', () => {})
+cmButton.addEventListener('click', () => {});
 
-elmoButton.addEventListener('click', () {})
+elmoButton.addEventListener('click', () => {});
 
+function clickLocation ()
+function pickRandomPlace () {
+    const places = ['bigBird'. 'cookiemonster', 'elmo'];
+}
+    const index = Math.floor(Math.random() * places.length);
+    const corrrectPlace = places[index];
+    return corrrectPlace;
 /* State */
 
 /* Events */

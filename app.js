f8693943799c76fd2190623f0036e4c88fcc6f63
Laurie Fish <iamlauriefish@gmail.com>
2023-01-13 16:01:23 -0800

    fixed all functions

--- app.js
+++ app.js
@@ -5,49 +5,60 @@ const bbButton = document.getElementById('bb-button');
 const cmButton = document.getElementById('cm-button');
 const elmoButton = document.getElementById('elmo-button');
 
-const bigBird = document.getElementById('bb');
-const cookiemonster = document.getElementById('cm');
-const elmo = document.getElementById('elmo');
+const bb = document.getElementById('bb-section');
+const cm = document.getElementById('cm-section');
+const elmo = document.getElementById('elmo-section');
 
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
-console.log('bb');
-
 
 let correctGuesses = 0;
 let totalGuesses = 0;
 let incorrectGuesses = 0;
 
-bbButton.addEventListener('click', () => {});
+bbButton.addEventListener('click', () => {
+    handleGuess('bb');
+});
 
-cmButton.addEventListener('click', () => {});
+cmButton.addEventListener('click', () => {
+    handleGuess('cm');
+});
 
-elmoButton.addEventListener('click', () => {});
+elmoButton.addEventListener('click', () => {
+    handleGuess('elmo');
+});
 
-function clickLocation ();
-function removeClasses ();
-const correctPlace = pickRandomPlace();
-const correctSectionEl = document.getElementById{correctPlace}-section);
+function handleGuess(placeTheUserClicked) {
+    removeClasses();
+    const correctPlace = pickRandomPlace();
+    const correctSectionEl = document.getElementById(`$correctPlace)-section1`);
+    correctSectionEl.classList.add('cookie');
 
-correctSectionEl.classList.add('cookie');
 
 if (correctPlace === placeTheUserClicked) {
     correctGuesses++;
 } else {
     incorrectGuesses++;
 }
-totalGuesses++
+totalGuesses++;
 
 winsEl.textContent = correctGuesses;
 lossesEl.textContent = incorrectGuesses;
 totalEl.textContent = totalGuesses;
 
-    function pickRandomPlace () {
-        const places = ['bigBird'. 'cookiemonster', 'elmo'];
-        const index = Math.floor(Math.random() * places.length);
-        const correctPlace = places[index];
-        return correctPlace;}
+function removeClasses() {
+    bb.classList.remove('cookie');
+    cm.classList.remove('cookie');
+    elmo.classList.remove('cookie');
+}
+
+function pickRandomPlace() {
+    const places = ['bb', 'cm', 'elmo'];
+    const index = Math.floor(Math.random() * places.length);
+    const correctPlace = places[index];
+    return correctPlace;
+}
 /* State */
 
 /* Events */
